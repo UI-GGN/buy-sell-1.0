d2f556109c9debd3f27828d5ee788dad52955b1d
Ankur <ankur.marwaha@rxglobal.com>
2022-07-06 13:44:39 +0530

    Added simple nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function openLogin(){
+
+}
+
+function openRegister(){
+
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="nav">
+      <button className="btn nav-button login-btn" onClick={openLogin}>Login</button>
+      <button className="btn nav-button register-btn" onClick={openRegister}>Register</button>
+    </nav>
   );
 }
 
