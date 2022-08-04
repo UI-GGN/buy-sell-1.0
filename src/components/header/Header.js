fc845cb5e9ef09535314a8b838ae8ae979c6632d
Madhankumar <madhan.kumar@thoughtworks.com>
2022-08-04 11:26:09 +0530

    [Madhan] Update header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,51 +1,162 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./header.css"
 import Button from "../custom/button/Button"
-import { Link } from "react-router-dom";
-import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from "react-router-dom";
+import Logo from "../../logo/CBP Marketplace-1.png";
+import SellersPortalLogo from "../../logo/CBP_Sellers_Portal.png"
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Dropdown from "../dropdown/Dropdown";
+import Searchbar from "../searchbar/Searchbar";
 
-const Header = ({isAuthenticated, login, register, onLogout}) => {
-    const [path, setPath] = useState(window.location.pathname);
+const Header = ({isAuthenticated, onLogout, setSearchQuery}) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const iconLocationArray = ["/", "/my/wishlist", "/my/cart"]
+    const navigate = useNavigate();
 
+    const routeHome = () => {
+        setShowDropdown(false);
+        window.location.pathname === "/" && setSearchQuery("");
+        navigate("/");
+    }
+
+    const routeLogin = () => {
+        setShowDropdown(false);
+        navigate("/login")
+    }
+
+    const routeRegister = () => {
+        setShowDropdown(false);
+        navigate("/register")
+    }
+
+    const routeSeller = () => {
+        setShowDropdown(false);
+        navigate("/seller")
+    }
+
+    const logout = () => {
+        setShowDropdown(false);
+        onLogout();
+    }
 
     const buttonSection = () => {
         return(
-            <div className="button-section">
-                <Button class="header-button" onClick={login}
-                >
-                    Login
-                </Button>
-                <Button class="header-button" onClick={register}
-                >
-                    Register
-                </Button>
+            <div>
+                <h5 className="dropdown-welcome">Welcome to CBP Marketplace</h5>
+                <div className="button-section">
+                    <Button className="header-button" onClick={routeLogin}
+                    >
+                        Login
+                    </Button>
+                    <Button className="header-button" onClick={routeRegister}
+                    >
+                        Sign Up
+                    </Button>
+                </div>
             </div>
         )
     }
 
-    const profileSection = () => {
+    const username = () => {
         return(
-            <div className="profile">
-                <p>{localStorage.getItem("user")}</p>
-                <button onClick={() => {onLogout()}}><LogoutIcon/></button>
+            <div>
+                <div className="welcome-user">
+                    <p>Hi</p>
+                    <p id="username">{localStorage.getItem("user")}</p>
+                </div>
+                <hr/>
+                <div><p>Orders</p></div>
+                <div><p>Wishlist</p></div>
+                <div><p>My Profile</p></div>
+                <div><p>Saved Addresses</p></div>
             </div>
+            
         )
     }
 
     return(
-        <nav className="header">
-           {/* <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" className="marketplace-icon"><title/>
-           <path d="M28.908,12.571a.952.952,0,0,0-.1-.166,3.146,3.146,0,0,0-.118-.423c-.006-.016-.012-.032-.02-.048L25.917,5.6A1,1,0,0,0,25,5H7a1,1,0,0,0-.917.6l-2.77,6.381a2.841,2.841,0,0,0,0,2.083A4.75,4.75,0,0,0,6,16.609V27a1,1,0,0,0,1,1H25a1,1,0,0,0,1-1V16.609a4.749,4.749,0,0,0,2.687-2.543,2.614,2.614,0,0,0,.163-.655A1.057,1.057,0,0,0,28.908,12.571ZM13,26V20h2v6Zm4,0V20h2v6Zm7,0H21V19a1,1,0,0,0-1-1H12a1,1,0,0,0-1,1v7H8V17a5.2,5.2,0,0,0,4-1.8,5.339,5.339,0,0,0,8,0A5.2,5.2,0,0,0,24,17Zm2.837-12.7A3.015,3.015,0,0,1,24,15a2.788,2.788,0,0,1-3-2.5,1,1,0,0,0-2,0A2.788,2.788,0,0,1,16,15a2.788,2.788,0,0,1-3-2.5,1,1,0,0,0-2,0A2.788,2.788,0,0,1,8,15a3.016,3.016,0,0,1-2.838-1.7.836.836,0,0,1,0-.571L7.656,7H24.344l2.477,5.7A.858.858,0,0,1,26.837,13.3Z"/>
-           </svg> */}
-           <Link
-           style={{textDecoration: 'none'}}
-           to={"/"}
-           >
-            <h1>CBP Marketplace</h1>
-            </Link> 
-           {console.log(window.location.pathname)}
-           {window.location.pathname === "/" && (!isAuthenticated ? buttonSection() : profileSection())}
-        </nav>
+        <div>
+            <nav className="header">
+            <button
+            className="icon-button"
+            style={{textDecoration: 'none', marginInlineStart: "20px"}}
+            onClick={() => routeHome()}
+            >
+                {(window.location.pathname === "/seller" || window.location.pathname === "/seller/register") 
+                ? 
+                <img className="sellers-logo" src={SellersPortalLogo} alt="Logo for CBP Sellers Portal"/> 
+                : 
+                <img className="logo" src={Logo} alt="CBP Marketplace Logo"/>}
+            </button>
+
+            {window.location.pathname === "/" &&
+            <Searchbar setSearchQuery={setSearchQuery}/>}
+            
+            {iconLocationArray.includes(window.location.pathname) &&
+            <div className="icons-section">
+                <div className="profile-icon">
+                    <button 
+                    className="icon-button"
+                    onClick={() => {setShowDropdown(!showDropdown)}}
+                    >
+                        <PersonOutlineOutlinedIcon/>
+                        {/* <img src="/Users/maddy/Desktop/Projects/CBP/icons8-customer-100.png"></img> */}
+                    </button>
+                    <p className="icon-title">Profile</p>
+                    {showDropdown &&
+                    <div
+                    style={{height: 0}}
+                    >
+                        <div className="indicator"></div>
+                        <Dropdown
+                        style={isAuthenticated ? {right: "80px"} : {right: "0px"}}
+                        setShowDropdown={setShowDropdown}
+                        >
+                            {isAuthenticated ? username() : buttonSection()}
+                            <hr/>
+                            <div onClick={() => routeSeller()}><p>Seller Portal</p></div>
+                            <div><p>Contact us</p></div>
+                            {isAuthenticated &&
+                            <div
+                            onClick={() => {logout()}}
+                            >
+                                <p>Logout</p>
+                            </div>}
+                        </Dropdown>
+                    </div>
+                    }
+                </div>
+
+                {isAuthenticated &&
+                <div className="inner-icons-section">
+                    <div className="wishlist-icon">
+                        <button 
+                        className="icon-button"
+                        onClick={() => navigate("/my/wishlist")}
+                        >
+                            <FavoriteBorderIcon/>
+                        </button>
+                        <p className="icon-title">Wishlist</p>
+                        {window.location.pathname === "/my/wishlist" && <div className="indicator"></div>}
+                    </div>
+                    <div className="cart-icon">
+                        <button 
+                        className="icon-button"
+                        onClick={() => navigate("/my/cart")}
+                        >
+                            <ShoppingCartOutlinedIcon/>
+                        </button>
+                        <p className="icon-title">Cart</p>
+                        {window.location.pathname === "/my/cart" && <div className="indicator"></div>}
+                    </div>
+                </div>
+                }
+            </div>
+            }
+            </nav>
+        </div>
     )
 }
 
