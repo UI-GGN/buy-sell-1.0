9a271367a5a590f26d9a670a23cc820a6c9a2651
Madhankumar <madhan.kumar@thoughtworks.com>
2022-08-04 11:29:09 +0530

    [Madhan] Update products component

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,31 +1,37 @@
 const productList = [
     {
         product: "HP-Notebook",
+        id: "1",
         image: "https://m.media-amazon.com/images/I/41KupVQ5l6L._AC_SY400_.jpg",
         price: "35000"
     },
     {
         product: "Slim-fit Casual Shirt",
+        id: "2",
         image: "https://m.media-amazon.com/images/I/51XatSFVktL._AC_UL320_.jpg",
         price: "499"
     },
     {
         product: "Fastrack Watch",
+        id: "3",
         image: "https://m.media-amazon.com/images/I/61FFBTzKiUL._AC_UL320_.jpg",
         price: "765"
     },
     {
         product: "ACCOX Regular-fit Cotton Shirt",
+        id: "4",
         image: "https://m.media-amazon.com/images/I/61e9TOC+nQL._AC_UL320_.jpg",
         price: "599"
     },
     {
         product: "Samsung S21 Mobile",
+        id: "5",
         image: "https://m.media-amazon.com/images/I/81kfA-GtWwL._AC_UL320_.jpg",
         price: "80999"
     },
     {
         product: "JBL Tune Headphone",
+        id: "6",
         image: "https://m.media-amazon.com/images/I/61kFL7ywsZS._AC_UY218_.jpg",
         price: "3499"
     }
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,21 +1,84 @@
-import React, { useState } from "react";
-import productList from "./ProductList";
+import React, { useEffect, useState } from "react";
 import './product.css';
+import * as JsSearch from "js-search";
+import productList from "./ProductList";
 import InfoDialog from "../dialog/infoDialog/InfoDialog";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-const Products = ({isAuthenticated}) => {
+const Products = ({isAuthenticated, searchQuery, setSearchQuery}) => {
     const [showDialog, setShowDialog] = useState(false);
+    const username = localStorage.getItem("username") || "";
+    const [wishlist, setWishlist] = useState(JSON.parse(localStorage.getItem("wishlist")) || "");
+    let params = (new URL(document.location)).searchParams;
+    
+    useEffect(() => {
+        setSearchQuery(params.get("s") || "")
+    })
+
+    let products = [...productList]
+
+    if(searchQuery !== ""){
+        let items = new JsSearch.Search("product");
+        items.addIndex("product");
+        items.addDocuments(productList);
+        products = items.search(searchQuery);
+    }
+
+    const updateWishlist = (product) => {
+        let updatedWishlist = {...wishlist}; 
+        let addProduct = true;
+        updatedWishlist[username].map((item, index) => {
+            if (item === product) {
+                updatedWishlist[username].splice(index, 1);
+                addProduct = false;
+            }
+        })
+        if (addProduct) updatedWishlist[username].push(product);
+        setWishlist(updatedWishlist);
+        localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
+    }
 
     return(
-        <>
+        <div className="product-container">
+            {searchQuery !== "" && <h2 className="results-title">Results</h2>}
             <section className="product-grid">
                 {
-                    productList.map((productItem, index) => {
+                    products.map((productItem, index) => {
                         return(
-                            <div className="product-card" key={index} onClick={()=>{setShowDialog(true)}}>
-                                <img src={productItem.image} alt={productItem.product + "image"}></img>
-                                <h3>{productItem.product}</h3>
-                                <p>{"Rs. " + productItem.price}</p>
+                            <div className="product-card" key={index}>
+                                <img
+                                src={productItem.image} 
+                                alt={productItem.product + " image"}
+                                className="product-link"
+                                onClick={()=>{setShowDialog(true)}}
+                                >
+                                </img>
+                                <hr/>
+                                <div className="product-info-container">
+                                    <div>
+                                        <h3
+                                        className="product-link"
+                                        onClick={()=>{setShowDialog(true)}}
+                                        >
+                                            {productItem.product}
+                                        </h3>
+                                        <p>{"Rs. " + productItem.price}</p>
+                                    </div>
+                                    {isAuthenticated && 
+                                    (wishlist[username].includes(productItem.id)) ? (
+                                        <FavoriteIcon
+                                        className="fav-icon"
+                                        style={{color: "red"}}
+                                        onClick={() => updateWishlist(productItem.id)}
+                                        />
+                                    ) : (
+                                        <FavoriteBorderIcon
+                                        className="fav-icon"
+                                        onClick={() => updateWishlist(productItem.id)}
+                                        />
+                                    )}
+                                </div>
                             </div>
                         )
                     })
@@ -28,7 +91,7 @@ const Products = ({isAuthenticated}) => {
             :
              <div></div>
             )}
-        </>
+        </div>
     )
 }
 
