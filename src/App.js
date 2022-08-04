ddbe52c095d00d857c94f59c9235fe39d67ec3ae
Madhankumar <madhan.kumar@thoughtworks.com>
2022-08-04 11:20:08 +0530

    [Madhan] Store data in localStorage while the app starts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
+import { Database, Wishlist, Cart } from "./mocks/Database";
 import RootRouter from "./components/router/Router";
 
 
 function App() {
+    localStorage.getItem("database") === null && localStorage.setItem("database", JSON.stringify(Database));
+    localStorage.getItem("wishlist") === null && localStorage.setItem("wishlist", JSON.stringify(Wishlist));
+    localStorage.getItem("cart") === null && localStorage.setItem("cart", JSON.stringify(Cart));
 
-  return(
-        <BrowserRouter >
-          <RootRouter/>
-        </BrowserRouter>
-  )
+    return(
+      <BrowserRouter >
+        <RootRouter/>
+      </BrowserRouter>
+    )
 }
 
 export default App;
