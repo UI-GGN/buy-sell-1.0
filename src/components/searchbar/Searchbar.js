ebc0ff08627fed5a1a83ae1162eb6e83a4d6bbb3
Madhankumar <madhan.kumar@thoughtworks.com>
2022-08-04 11:14:43 +0530

    [Madhan] Add searchbar component

---
+++ src/components/searchbar/Searchbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import "./searchbar.css"
+import { useNavigate } from "react-router-dom";
+
+const Searchbar = ({setSearchQuery}) => {
+    const navigate = useNavigate();
+    const submitSearch = (event) => {
+        event.preventDefault();
+        setSearchQuery(event.target[0].value);
+        navigate("/?s="+event.target[0].value);
+    }
+
+    return(
+        <div className="search-container">
+            <form onSubmit={(e) => submitSearch(e)}
+            >
+                <div className="search-bar-container">
+                    <input
+                    className="search-input"
+                    type="text"
+                    placeholder="Search for products"
+                    name="s"
+                    />
+                    <button className="search-button">
+                        <SearchIcon/>
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Searchbar;
